ddd12a6c9979a0ff10783ef31f419524734d639a
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2024-04-20 12:22:19 +0300

    Render a window with nothing in it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+  env_logger::Builder::new().filter_level(log::LevelFilter::Info).init();
+  let options = eframe::NativeOptions {
+    viewport: egui::ViewportBuilder::default().with_maximized(true),
+    ..Default::default()
+  };
+  eframe::run_native(
+    "Color editor",
+    options,
+    Box::new(|_| { Box::<ColorEditor>::default() }),
+  )
+}
+
+struct ColorEditor {
+}
+
+impl Default for ColorEditor {
+  fn default() -> Self {
+    Self {
+    }
+  }
+}
+
+impl eframe::App for ColorEditor {
+  fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    egui::CentralPanel::default().show(ctx, |_ui| {
+    });
+  }
 }
